f1c9f0f64ca25fd1bbc4ec401bb1a0fb28c8f404
Sashabatunin <36814971+Sashabatunin@users.noreply.github.com>
2026-05-05 21:09:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,8 +50,8 @@ async function init() {
 
   // 2. Load face‑api models
   try {
-    await faceapi.nets.tinyFaceDetector.load('/models');
-    await faceapi.nets.faceLandmark68TinyNet.load('/models'); // may be optional, but we load it
+    await faceapi.nets.tinyFaceDetector.load('turret/models');
+    await faceapi.nets.faceLandmark68TinyNet.load('turret/models'); // may be optional, but we load it
     statusDiv.textContent = 'Face models loaded. Connecting MQTT…';
     btnFace.disabled = false;
     btnCustom.disabled = false;
